090347d596de1768b7f19a342e91a4ce44c32e3f
Arthur <embryarthur@gmail.com>
2023-02-13 18:43:15 -0600

    ping for client

--- commit.sh
+++ commit.sh
@@ -7,8 +7,5 @@ git push
 
 
 # find all key value pairs in app.env and commit with gh secret set foo -b bar
-cat app.env | while read line; do
-    key=$(echo $line | cut -d '=' -f 1)
-    value=$(echo $line | cut -d '=' -f 2)
-    gh secret set $key -b $value
-done
+
+gh secret set "test" -b "test"
