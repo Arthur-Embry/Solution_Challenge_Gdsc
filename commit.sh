fb2dbd9cb2e1a26bfdeebfced18d92e69ae39d8d
Arthur <embryarthur@gmail.com>
2023-02-13 18:48:46 -0600

    ping for client

--- commit.sh
+++ commit.sh
@@ -13,16 +13,12 @@ cat app.env | while read line; do
     gh secret set $key -b $value
 done
 
-input="./app.env"
-while IFS= read -r line
-do
-    parts=($line)
-    key=${parts[0]}
-    value=$(echo "$line" | sed 's/KEY[[:digit:]]*[[:space:]]//')
-    echo .
-    echo "Key: [$key]"
-    echo "Value: [$value]"
-    echo .
-done < "$input"
+# Read all lines from the file "app.env"
+while read -r line; do
+  # Split the line into key and value
+  key=$(echo $line | cut -d'=' -f1)
+  value=$(echo $line | cut -d'=' -f2)
 
-gh secret set "test" -b "test"
+  # Run the "gh secret set" command with the key and value
+  gh secret set $key -b $value
+done < app.env
