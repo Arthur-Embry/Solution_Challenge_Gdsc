ab0bdd1c08de0ba0f857f24809a1fdc221aa7051
username <email@example.com>
2023-02-22 15:57:15 -0600

    testing update to docs

--- app/api/demo_services.py
+++ app/api/demo_services.py
@@ -9,7 +9,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 class foo_params(BaseModel):
     echo: Union[str,None]="",
-    
+
 
 def foo_run(params: foo_params):
     if(params.echo==None):
@@ -21,6 +21,15 @@ class bar_params(BaseModel):
     echo: Union[str,None]="",
 
 def bar_run(params: bar_params):
+    if(params.echo==None):
+        return "hello world"
+    else:
+        return params.echo
+
+class baz_params(BaseModel):
+    echo: Union[str,None]="",
+
+def baz_run(params: baz_params):
     if(params.echo==None):
         return "hello world"
     else:
